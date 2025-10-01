3651e9923d41cc53fcfb1998f9282b20461e4f78
SauloFeitosa244 <161551217+SauloFeitosa244@users.noreply.github.com>
2025-10-01 19:01:08 +0000

    Início do case 1 do menu principal

--- ProjetoEscola/escola.c
+++ ProjetoEscola/escola.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define MAX_ALUNOS 5
+
+typedef struct Alunos{
+    int matricula;
+    char nome[255];
+    int sexo;
+    //data de nascimento
+    //CPF <- FAZER VALIDADOR DE CPF
+} Aluno;
+
 int main(){
-    
+    Aluno listaAlunos[MAX_ALUNOS];
+
     bool sair = false;
+    bool voltar = false;
+    
+    int qtdAlunos = 0;
     int opcao;
     
     
     while(!sair){
-        printf("Projeto Escola\n");
+        printf("----Projeto Escola----\n");
         printf("0 - Sair\n");
         printf("1 - Aluno\n");
         printf("2 - Professor\n");
         printf("3 - Disciplina\n");
         scanf("%d", &opcao);
+        voltar = false;
 
         switch(opcao){
             case 0:
@@ -21,7 +36,38 @@ int main(){
                 sair = true;
                 break;
             case 1:
-                //Adicionar função aluno
+                int opcaoAluno;
+                while(!voltar){
+                    printf("====MENU ALUNO====\n");
+                    printf("0 - Voltar\n");
+                    printf("1 - Cadastrar Aluno\n");
+                    printf("2 - Listar Alunos\n");
+                    printf("3 - Atualizar Aluno\n");
+                    printf("4 - Remover Aluno\n");
+                    scanf("%d", &opcaoAluno);
+                    
+                    switch (opcaoAluno){
+                    case 0:
+                        voltar = true;
+                        break;
+                    case 1:
+                        printf("Digite a matrícula do aluno: ");
+                        scanf("%d", &listaAlunos[qtdAlunos].matricula);
+                        //printf("Digite o nome do aluno: ");
+                        //pegar Nome do aluno com espaços
+                        printf("Digite o sexo do aluno\n");
+                        printf("1 Masculino  | 2 - Feminnino\n");
+                        scanf("%d", &listaAlunos[qtdAlunos].sexo);
+                        //printf("Digite a data de nascimento do aluno: ");
+                        //PEGAR DATA DE NASCIMENTO COM FORMATO DD/MM/AAAA
+                        //printf("Digite o CPF do aluno: ");
+                        //PEGAR CPF COM FORMATO XXX.XXX.XXX-XX
+                        printf("Aluno cadastrado!\n");
+                        qtdAlunos++;
+                        break;
+                    }
+                }
+                break;
             case 2:
                 //Adicionar função professor
             case 3:
