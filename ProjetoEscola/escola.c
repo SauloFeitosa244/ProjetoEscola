eb9116292d6ea5178358a4cda1d6d00e00ddc86d
SauloFeitosa244 <161551217+SauloFeitosa244@users.noreply.github.com>
2025-10-06 20:57:11 +0000

    adiçao das funções listarAlunos e cadastrarAluno

--- ProjetoEscola/escola.c
+++ ProjetoEscola/escola.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define MAX_ALUNOS 5
+#define MAX_ALUNOS 2
 
 typedef struct Alunos{
     int matricula;
@@ -13,11 +13,13 @@ typedef struct Alunos{
     int cpf[11];
 } Aluno;
 
+int listarAlunos(int qtdAlunos, Aluno listaAlunos[MAX_ALUNOS]);
+int cadastrarAluno(int qtdAlunos,Aluno listaAlunos[MAX_ALUNOS]);
+int menuPrincipal();
+int menuAluno();
+
 int main(){
-
-    int menuPrincipal();
-    void menuAluno();
-
+    
     Aluno listaAlunos[MAX_ALUNOS];
 
     bool sair = false;
@@ -40,8 +42,7 @@ int main(){
             case 1:
                 int opcaoAluno;
                 while(!voltar){
-                    menuAluno();
-                    scanf("%d", &opcaoAluno);
+                    opcaoAluno = menuAluno();
                     
                     switch (opcaoAluno){
                     case 0:
@@ -49,46 +50,26 @@ int main(){
                         voltar = true;
                         break;
                     case 1:
-                        printf("Digite a matrícula do aluno: ");
-                        scanf("%d", &listaAlunos[qtdAlunos].matricula);
-                        getchar();
-
-                        printf("Digite o nome do aluno: ");
-                        fgets(listaAlunos[qtdAlunos].nome, 255, stdin);
-
-                        printf("Digite o sexo do aluno: ");
-                        printf("1 Masculino  | 2 - Feminnino\n");
-                        scanf("%d", &listaAlunos[qtdAlunos].sexo);
-
-                        printf("Digite a data de nascimento do aluno: ");
-                        scanf("%d/%d/%d", &listaAlunos[qtdAlunos].dia, &listaAlunos[qtdAlunos].mes, &listaAlunos[qtdAlunos].ano);
-                        //printf("Digite o CPF do aluno: ");
-                        //PEGAR CPF COM FORMATO XXX.XXX.XXX-XX
-                        printf("Aluno cadastrado!\n\n");
-                        qtdAlunos++;
+                        qtdAlunos = cadastrarAluno(qtdAlunos, listaAlunos);
                         break;
                     case 2:
-                        printf("Lista de Alunos:\n");
-                        for(int i = 0;i < qtdAlunos; i++){
-                            printf("\n----Aluno %d----\n", i+1);
-                            printf("Nome: %s", listaAlunos[i].nome);
-                            printf("Matrícula: %d\n", listaAlunos[i].matricula);
-                            printf("Data de nascimento: %d/%d/%d\n", listaAlunos[i].dia, listaAlunos[i].mes, listaAlunos[i].ano);
-                            if(listaAlunos[i].sexo == 1)
-                                printf("Sexo: Masculino\n");
-                            else if(listaAlunos[i].sexo == 2)
-                                printf("Sexo: Feminino\n");
-                            
-                        }
+                        listarAlunos(qtdAlunos, listaAlunos);
+                        break;
+                        
+                    default:
+                        printf("\nOpção inválida\n\n");
+                        break;
                     }
                 }
                 break;
             case 2:
                 //Adicionar função professor
+                break;
             case 3:
                //Adicionar função disciplina
+               break;
             default:
-                printf("\nOpção inválida\n");
+                printf("\nOpção inválida\n\n");
                 break;
         }
     }
@@ -105,14 +86,70 @@ int menuPrincipal(){
     printf("2 - Professor\n");
     printf("3 - Disciplina\n");
     scanf("%d", &opcao);
+    return opcao;
 }
 
-void menuAluno(){
+int menuAluno(){
+    int opcaoAluno;
     printf("====MENU ALUNO====\n");
     printf("0 - Voltar\n");
     printf("1 - Cadastrar Aluno\n");
     printf("2 - Listar Alunos\n");
     printf("3 - Atualizar Aluno\n");
     printf("4 - Remover Aluno\n");
+    scanf("%d", &opcaoAluno);
+    return opcaoAluno;
 }
 
+
+int cadastrarAluno(int qtdAlunos, Aluno listaAlunos[MAX_ALUNOS]){
+    if(qtdAlunos < MAX_ALUNOS){
+    printf("Digite a matrícula do aluno: ");
+    scanf("%d", &listaAlunos[qtdAlunos].matricula);
+    getchar();
+
+    printf("Digite o nome do aluno: ");
+    fgets(listaAlunos[qtdAlunos].nome, 255, stdin);
+        
+    printf("Digite o sexo do aluno: \n ");
+    printf("1 Masculino  | 2 - Feminnino: ");
+    scanf("%d", &listaAlunos[qtdAlunos].sexo);
+    
+    if(listaAlunos[qtdAlunos].sexo != 1 && listaAlunos[qtdAlunos].sexo != 2){
+        while(listaAlunos[qtdAlunos].sexo != 1 && listaAlunos[qtdAlunos].sexo != 2){
+        printf("Opção inválida\n");
+        printf("Digite o sexo do aluno: \n ");
+        printf("1 Masculino  | 2 - Feminnino: ");
+        scanf("%d", &listaAlunos[qtdAlunos].sexo);
+        }
+    }
+    
+    printf("Digite a data de nascimento do aluno (dd/mm/aaaa): ");
+    scanf("%d/%d/%d", &listaAlunos[qtdAlunos].dia, &listaAlunos[qtdAlunos].mes, &listaAlunos[qtdAlunos].ano);
+    //printf("Digite o CPF do aluno: ");
+    //PEGAR CPF COM FORMATO XXX.XXX.XXX-XX
+    printf("Aluno cadastrado!\n\n");
+    qtdAlunos++;
+    return qtdAlunos;
+    } else{
+        printf("\nQuantidade de alunos cadastrados excedida\n");
+        printf("Exclua ou atualize para adicionar dados novos\n\n");
+        return qtdAlunos;
+    }
+}
+
+int listarAlunos(int qtdAlunos, Aluno listaAlunos[MAX_ALUNOS]){
+    printf("Lista de Alunos:\n");
+    for(int i = 0; i < qtdAlunos; i++){
+        printf("\n----Aluno %d----\n", i+1);
+        printf("Nome: %s", listaAlunos[i].nome);
+        printf("Matrícula: %d\n", listaAlunos[i].matricula);
+        printf("Data de nascimento: %d/%d/%d\n", listaAlunos[i].dia, listaAlunos[i].mes, listaAlunos[i].ano);
+        if(listaAlunos[i].sexo == 1)
+            printf("Sexo: Masculino\n");
+        else if(listaAlunos[i].sexo == 2)
+            printf("Sexo: Feminino\n");
+        
+    }
+    
+}
